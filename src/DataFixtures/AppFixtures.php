81502d42d5454cb7f246b48537d0a8058dbcd6ae
Clément Cazaud <clement.cazaud@gmail.com>
2021-07-14 13:52:11 +0200

    Implements data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,93 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Spec\BusTripStepSpec;
+use App\Entity\Spec\PlaneTripStepSpec;
+use App\Entity\Spec\TrainTripStepSpec;
+use App\Entity\Spec\TripSpec;
+use App\Entity\Spec\TripStepSpecCollection;
+use App\Service\Message\CreateTripCommand;
+use App\Service\TripServiceInterface;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * @package App\DataFixtures
+ */
 class AppFixtures extends Fixture
 {
+    /**
+     * @var TripServiceInterface
+     */
+    private TripServiceInterface $tripService;
+
+    /**
+     * @param TripServiceInterface $tripService
+     */
+    public function __construct(TripServiceInterface $tripService)
+    {
+        $this->tripService = $tripService;
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $tripStepSpecs = [];
 
-        $manager->flush();
+        $tripStepSpecs[] = new TrainTripStepSpec(
+            'T2',
+            'Montpellier',
+            'Nîmes',
+            new DateTime(),
+            new DateTime(),
+            '1A'
+        );
+
+        $tripStepSpecs[] = new TrainTripStepSpec(
+            'T5',
+            'Aix-en-Provence',
+            'Nice',
+            new DateTime(),
+            new DateTime(),
+            '1A'
+        );
+
+        $tripStepSpecs[] = new PlaneTripStepSpec(
+            'T1',
+            'Moscou',
+            'Montpellier',
+            new DateTime(),
+            new DateTime(),
+            '1A',
+            'A1',
+            'A'
+        );
+
+        $tripStepSpecs[] = new BusTripStepSpec(
+            'T4',
+            'Marseille',
+            'Aix-en-Provence',
+            new DateTime(),
+            new DateTime(),
+            '1A'
+        );
+
+        $tripStepSpecs[] = new TrainTripStepSpec(
+            'T3',
+            'Nîmes',
+            'Marseille',
+            new DateTime(),
+            new DateTime(),
+            '1A'
+        );
+
+        $tripSpec = new TripSpec(new TripStepSpecCollection($tripStepSpecs));
+
+        $command = new CreateTripCommand($tripSpec);
+
+        $this->tripService->createTrip($command);
     }
 }
